7e62795450e8448dd4265d77e45869976d286618
Cristian David Franco Garcia <cristiandavidfrancogarcia@gmail.com>
2025-07-05 10:36:12 +0200

    chore: Add domain layer with entity Task

--- src/domain/entities/Task.ts
+++ src/domain/entities/Task.ts
@@ -1,12 +1,12 @@
 export interface Task {
-    id?: string;
-    status: 'pending' | 'completed' | 'failed';
-    price: number;
-    createdAt?: Date;
-    updatedAt?: Date;
-    originalPath: string;
-    images: {
-        resolution: string;
-        path: string;
-    }[];
+  id?: string;
+  status: 'pending' | 'completed' | 'failed'; // Using a union type for status
+  price: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  originalPath: string;
+  images: {
+    resolution: string;
+    path: string;
+  }[];
 }
