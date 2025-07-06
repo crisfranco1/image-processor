0bff693ce23d715b43f1e5b7dce3cb6abffafd03
Cristian David Franco Garcia <cristiandavidfrancogarcia@gmail.com>
2025-07-06 14:22:05 +0200

    test: new test for function to process image from an URL

--- __tests__/unit/CreateTaskUseCase.test.ts
+++ __tests__/unit/CreateTaskUseCase.test.ts
@@ -37,7 +37,31 @@ describe('CreateTaskUseCase', () => {
         expect(result).toEqual(createdTask);
     });
 
+    it('should create a new task from a URL originalPath and return it', async () => {
+        const url = 'https://example.com/image.jpg';
+        const command: CreateTaskCommand = {
+            originalPath: url
+        };
+        const createdTask: Task = {
+            taskId: 'url123',
+            status: 'pending',
+            price: 10,
+            originalPath: url,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            images: [],
+        };
+        mockTaskRepository.createTask.mockResolvedValue(createdTask);
+        const result = await createTaskUseCase.execute(command);
+        expect(mockTaskRepository.createTask).toHaveBeenCalledWith(expect.objectContaining({
+            originalPath: command.originalPath
+        }));
+        expect(result).toEqual(createdTask);
+    });
+
     it('should throw an error if originalPath is missing', async () => {
         await expect(createTaskUseCase.execute({})).rejects.toThrow('Missing required fields');
     });
+
+
 });
